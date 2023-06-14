2dee0006ba2023665781ba1807502a6618565dee
sam-the-programmer <85027459+sam-the-programmer@users.noreply.github.com>
2023-06-14 19:37:31 +0100

    reset for new design

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: "docs",
-    minify: true
-  }
-})
+	plugins: [sveltekit()]
+});
