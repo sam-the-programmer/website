2045f8a0afd50f3c047065ed0f6b9fbd0f2dfa8b
Sam <85027459+s4m-mo@users.noreply.github.com>
2026-07-31 12:19:23 +0100

    Add tutoring website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,35 @@ import { defineConfig, fontProviders } from "astro/config";
 
 import solidJs from "@astrojs/solid-js";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [solidJs()],
-    adapter: cloudflare(),
+    // adapter: cloudflare(),
+
+    output: "static",
 
     vite: {
         plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "@assets": "src/assets",
+                "@components": "src/components",
+                "@layouts": "src/layouts",
+                "@styles": "src/styles",
+                "@utils": "src/utils",
+            },
+        },
     },
 
     fonts: [
         {
             provider: fontProviders.fontsource(),
-            name: "Prata",
-            cssVariable: "--font-display",
+            name: "Geist Mono",
+            cssVariable: "--font-base",
         },
     ],
 });
